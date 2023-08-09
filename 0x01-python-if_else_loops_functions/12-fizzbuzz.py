25604292faf3d222332247b667b67a2d00420754
hajarmos <hajarbouzaid@gmail.com>
2023-08-09 02:01:00 +0000

    task12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    f = Fizz
+    b = Buzz
+    for i in range(1, 101):
+        if not(i % 3) and not(i %5):
+            print('{}{}'.format(f, b), end=' ')
+        elif not(i % 3):
+            print('{}'.format(f), end=' ')
+        elif not(i % 5):
+            print('{}'.format(b), end=' ')
+        else:
+            print('{}'.format(i), end=' ')
