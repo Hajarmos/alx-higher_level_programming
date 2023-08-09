75b43cd7119beba1d3510e67e270e81b088ece93
hajarmos <hajarbouzaid@gmail.com>
2023-08-09 20:37:16 +0000

    up

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('Z'), ord('A') - 1, -1):
-    if not(i % 2):
+    if not (i % 2):
         c = chr(i + 32)
     else:
         c = chr(i)
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@ def fizzbuzz():
     f = 'Fizz'
     b = 'Buzz'
     for i in range(1, 101):
-        if not(i % 3) and not(i % 5):
+        if not (i % 3) and not (i % 5):
             print('{}{}'.format(f, b), end=' ')
-        elif not(i % 3):
+        elif not (i % 3):
             print('{}'.format(f), end=' ')
-        elif not(i % 5):
+        elif not (i % 5):
             print('{}'.format(b), end=' ')
         else:
             print('{}'.format(i), end=' ')
