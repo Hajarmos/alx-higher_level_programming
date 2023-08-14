f7cc28892397c097200fe7a719729e7a9b04099e
hajarmos <hajarbouzaid@gmail.com>
2023-08-14 20:51:57 +0000

    task3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev = my_list[len(my_list) - 1::-1]
-    for j in rev:
-        print('{:d}'.format(j))
+    if my_list is not None:
+        rev = my_list[len(my_list) - 1::-1]
+        for j in rev:
+            print('{:d}'.format(j))
--- 0x03-python-data_structures/m.py
+++ 0x03-python-data_structures/m.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = []
+my_list = [5,8,96,56,2]
 print_reversed_list_integer(my_list)
 
