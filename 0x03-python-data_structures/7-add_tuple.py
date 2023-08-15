f96237437788828f48482added1e1452ee9d259a
hajarmos <hajarbouzaid@gmail.com>
2023-08-15 11:14:22 +0000

    task7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a is None or tuple_b is None:
-        return
-    if type(tuple_a) is tuple and type(tuple_b) is tuple:
-        la = len(tuple_a)
-        lb = len(tuple_b)
-        x = (0,)
-        if la >= 2 or lb >= 2:
-            tuple_a = tuple_a[:2]
-            tuple_b = tuple_b[:2]
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        if la == 1 or lb == 1:
-            if la == 1:
-                tuple_a = tuple_a + x
-            if lb == 1:
-                tuple_b = tuple_b + x
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        if la == 0:
-            tuple_a = x + x
-        if lb == 0:
-            tuple_b = x + x
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    else:
-        return
+    tuple_empty = ()
+
+    if tuple_a == tuple_empty:
+        tuple_a = (0, 0)
+    if tuple_b == tuple_empty:
+        tuple_b = (0, 0)
+    la = len(tuple_a)
+    lb = len(tuple_b)
+    if la == 1:
+        tuple_a = (tuple_a[0], 0)
+    if lb == 1:
+        tuple_b = (tuple_b[0], 0)
+
+    tup = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return tup
