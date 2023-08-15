eb0ca24cea06d14a897b8d7dee328985e35dd8a1
hajarmos <hajarbouzaid@gmail.com>
2023-08-15 13:23:12 +0000

    task13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_palindrome - check if list is palindrome
+ * @head: head list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head, *temp_t;
+	unsigned int len = 0, lent, lentt;
+
+	if (!head)
+		return (1);
+	while (temp)
+	{
+		len ++;
+		temp = temp->next;
+	}
+	temp = *head;
+	lent = len;
+	while (lent >= len / 2)
+	{
+		temp_t = *head;
+		lentt = lent;
+		while (lentt - 1)
+		{
+			temp_t = temp_t->next;
+			lentt--;
+		}
+		if (temp->n == temp_t->n)
+		{
+			temp = temp->next;
+			lent--;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
