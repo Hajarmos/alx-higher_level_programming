a85b923896927015b3036e2a4ef691c066667da6
hajarmos <hajarbouzaid@gmail.com>
2023-08-29 23:50:27 +0000

    size exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" class Square that defines a square """
+
+
+class Square:
+    """ class Square """
+
+    def __init__(self, size=0):
+        """ initial new square 
+        args:
+             size (int): the size of new square
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
