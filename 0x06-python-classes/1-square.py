efebc12eaebe05829263df86348377cf95f9d85d
hajarmos <hajarbouzaid@gmail.com>
2023-08-29 23:34:25 +0000

    square whith prv size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,12 @@ class Square that defines a square by: (based on 0-square.py)
 
 
 class Square:
-""" class Square """
+    """ class Square """
 
+    def __init__(self, size):
+        """ initial a new square
+        args:
+            size (int): The size of a square
+        """
 
+        self.__size = size
