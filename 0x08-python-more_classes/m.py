8731d9b358237f18d790e95e438175afc2b18b84
hajarmos <hajarbouzaid@gmail.com>
2023-09-04 22:08:22 +0000

    add atributes

--- 0x08-python-more_classes/m.py
+++ 0x08-python-more_classes/m.py
@@ -8,3 +8,21 @@ my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
 
+my_rectangle.width = -9
+my_rectangle.height = -9
+print(my_rectangle.__dict__)
+my_rectangle.width = dd
+my_rectangle.height = v
+print(my_rectangle.__dict__)
+my_rectangle.width = 10
+my_rectangle.height = -5
+print(my_rectangle.__dict__)
+my_rectangle.width = 10
+my_rectangle.height = v
+print(my_rectangle.__dict__)
+my_rectangle.width = -10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
+my_rectangle.width = r
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
