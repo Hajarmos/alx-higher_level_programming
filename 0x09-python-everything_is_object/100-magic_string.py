7c43cec46be8f710c774b86e7defce8dc0697960
hajarmos <hajarbouzaid@gmail.com>
2023-09-06 22:14:12 +0000

    task31

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.i = getattr(magic_string, 'i', 0) + 1
+    magic_string.i = getattr(magic_string, 'i', 1) + 1
+    print(magic_string.i)
     return ("BestSchool, " * (magic_string.i - 1) + "BestSchool")
