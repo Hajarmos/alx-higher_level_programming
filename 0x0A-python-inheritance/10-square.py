b2a4936b5066341f7a8ebcd3100b1140f7621f6f
hajarmos <hajarbouzaid@gmail.com>
2023-09-12 01:46:26 +0000

    task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle (9-rectangle.py)"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+
+    def __init__(self, size):
+        """ init new square
+        arg:
+            size (int): square size
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
