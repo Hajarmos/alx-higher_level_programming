0b69196317c768b00474aac274191be38d4c836e
hajarmos <hajarbouzaid@gmail.com>
2023-09-13 21:06:13 +0000

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" function that writes a string to a text file (UTF8)
+and returns the number of characters written"""
+
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file
+    returns the number of characters written
+    args:
+        filename: file name
+        text (str): string
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
