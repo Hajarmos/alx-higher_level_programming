37e8e55ce50c08ebe75a26b4f97410fd1e5cb0e5
hajarmos <hajarbouzaid@gmail.com>
2023-09-14 00:24:58 +0000

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and then save them to a file"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+filen = 'add_item.json'
+
+try:
+    con = load_from_json_file(filen)
+except:
+    con = []
+
+for i in range(1, len(sys.argv)):
+    con.append(sys.argv[i])
+
+save_to_json_file(con, filen)
